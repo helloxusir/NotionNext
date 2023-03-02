9e2233c0e8712726922a211ab79d604146953009
helloxusir <1986321321@qq.com>
2023-03-02 17:59:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.yizhuchi.com', // 网站地址
   KEYWORDS: 'ChatGPT, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: 'helloxusir@gmail.com', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_EMAIL: '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: '', // 你的微博个人主页
   CONTACT_TWITTER: '', // 你的twitter个人主页
   CONTACT_GITHUB: '', // 你的github个人主页 例如 https://github.com/tangly1024
